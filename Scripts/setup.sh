87f62b61bb944a0fbf725262fa203da5ce19b706
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-06-29 11:26:27 +0800

    support swift setup & build

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -2,79 +2,90 @@
 # owner: SamuelZhaoY
 
 ### Usage
-###		`make setup ARGS="$name-of-framework-to-create"`
+###        `make setup ARGS="--framework-name=$framework-to-create --language=$language"`
 ### Description
-###		execute the command to create a framework project in the current directory, it is suggeted each framework project should have its own working directory.
+###        execute the command to create a framework project in the current directory, it is suggeted each framework project should have its own working directory.
 ### Options
-###		<s>	:the name of the framework project to setup
+### Options
+###        --framework-name <s>    :the framework name, also used as the build target name.
+###        --language <s>    :the language to use.
 
 source "$(dirname $0)/environment.sh"
 
-if [ $# != 1 ] ; then
-	outputError 'Invalid parameters' 1
-fi
+appname=""
+bundle=""
 
-if [ -d $(pwd)/$1 ]; then
-	outputError "The directory $(pwd)/$1 already exists, please setup in an empty directory" 1
-fi
+for arg in "$@"; do
+case "$arg" in
+--framework-name=*) appname="${arg#*=}" ;;
+--language=*) bundle="${arg#*=}" ;;
+*) break ;;
+esac
+shift
+done
 
-appname=$1
+if [ "$bundle" = "swift" ] ; then
+bundle="Template-Swift.bundle"
+else
+bundle="Template.bundle"
+fi
 
 function proc_file_content() {
-	sed "s/Demo/${appname}/g" $1 > $1'.tmp'
-	rm -f $1
-	mv $1'.tmp' $1
+sed "s/Demo/${appname}/g" $1 > $1'.tmp'
+rm -f $1
+mv $1'.tmp' $1
 }
 
 function make_target() {
-	target=`echo $1 | sed "s/Demo/${appname}/g"`
-	target_length=${#target}
-	proj=$(dirname $0)/../Templates/Template.bundle
-	proj_length=${#proj}+1
-	target_length=$target_length-$proj_length
-	target=${target:proj_length:target_length}
-	echo $target
+target=`echo $1 | sed "s/Demo/${appname}/g"`
+target_length=${#target}
+proj=$(dirname $0)/../Templates/$bundle
+proj_length=${#proj}+1
+target_length=$target_length-$proj_length
+target=${target:proj_length:target_length}
+echo $target
 }
 
 function replace_filename() {
 
-	source=$1
-	target=`make_target "$1"`
+source=$1
+target=`make_target "$1"`
 
-	if [ -d "$source" ] ; then
-		mkdir -p $target
-	else
-		cp $source $target
-	fi
+if [ -d "$source" ] ; then
+mkdir -p $target
+else
+cp $source $target
+fi
 
-	filter_content_for_type 'project.pbxproj'
-	filter_content_for_type 'xcscheme'
-	filter_content_for_type '-Info.plist'
-	filter_content_for_type '.podspec.json'
-	filter_content_for_type '.h'
-	filter_content_for_type '.m'
-	filter_content_for_type 'Podfile'
-	filter_content_for_type 'README.md'
-	filter_content_for_type 'CHANGELOG.md'
+filter_content_for_type 'project.pbxproj'
+filter_content_for_type 'xcscheme'
+filter_content_for_type '-Info.plist'
+filter_content_for_type '.podspec.json'
+filter_content_for_type '.h'
+filter_content_for_type '.m'
+filter_content_for_type '.swift'
+filter_content_for_type 'Podfile'
+filter_content_for_type 'README.md'
+filter_content_for_type 'CHANGELOG.md'
 }
 
 function filter_content_for_type() {
-	type=$1
-	position=${#target}-${#type}
-	length=${#type}
-	if [ ${#target} -ge ${#type} ] && [ ${target:position:length} == $type ] ; then
-		proc_file_content $target
-	fi
+type=$1
+position=${#target}-${#type}
+length=${#type}
+if [ ${#target} -ge ${#type} ] && [ ${target:position:length} == $type ] ; then
+proc_file_content $target
+fi
 }
 
 function list() {
-	for file in `ls $1`
-	do
-		replace_filename $1/$file
-		if [ -d "$1/$file" ] ; then
-			list "$1/$file";
-		fi
-	done
+for file in `ls $1`
+do
+replace_filename $1/$file
+if [ -d "$1/$file" ] ; then
+list "$1/$file";
+fi
+done
 }
 
-list $(dirname $0)/../Templates/Template.bundle
+list $(dirname $0)/../Templates/$bundle
---
+++ Templates/Template-Swift.bundle/Demo/Demo.h
@@ -0,0 +1,17 @@
+//
+//  Demo.h
+//  Demo
+//
+//  Created by SamuelZhaoY on 28/6/18.
+//  Copyright © 2018 marmot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Demo.
+FOUNDATION_EXPORT double DemoVersionNumber;
+
+//! Project version string for Demo.
+FOUNDATION_EXPORT const unsigned char DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo/PublicHeader.h>
