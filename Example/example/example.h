e53c98b75df476bc2a2d054d145818af2a5cd903
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-07-05 17:58:23 +0800

    provide default project demo

---
+++ Example/example/example.h
@@ -0,0 +1,17 @@
+//
+//  example.h
+//  example
+//
+//  Created by SamuelZhaoY on 28/6/18.
+//  Copyright © 2018 marmot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for example.
+FOUNDATION_EXPORT double exampleVersionNumber;
+
+//! Project version string for example.
+FOUNDATION_EXPORT const unsigned char exampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <example/PublicHeader.h>
